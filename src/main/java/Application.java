54daedb2ed556e7f8d06065354b90206c48bf15b
Jordi <jordi.lanau@yahoo.com>
2024-07-14 11:15:19 +0200

    Add first request

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import todo.TodoClient;
+
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        TodoClient todoClient = new TodoClient();
+        System.out.println(todoClient.findAll());
+
+    }
+
+}
