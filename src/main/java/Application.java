8190fa8bc7f75bdf7ccb599ef3489e1882b848c9
Jordi <jordi.lanau@yahoo.com>
2024-07-14 11:25:30 +0200

    Add Todo Record

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,17 @@
+import todo.Todo;
 import todo.TodoClient;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
         TodoClient todoClient = new TodoClient();
-        System.out.println(todoClient.findAll());
+        List<Todo> todos = todoClient.findAll();
+
+        System.out.println(todos);
 
     }
 
--- src/main/java/todo/TodoClient.java
+++ src/main/java/todo/TodoClient.java
@@ -1,26 +1,37 @@
 package todo;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class TodoClient {
 
     private final String BASE_URL = "https://jsonplaceholder.typicode.com/todos";
     private final HttpClient client;
+    private final ObjectMapper objectMapper;
 
     public TodoClient() {
         this.client = HttpClient.newHttpClient();
+        objectMapper = new ObjectMapper();
     }
 
-    public String findAll() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(BASE_URL)).GET().build();
+    public List<Todo> findAll() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create(BASE_URL))
+                .GET()
+                .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        return response.body();
+        return objectMapper.readValue(response.body(), new TypeReference<List<Todo>>() {
+        });
     }
 
 }
